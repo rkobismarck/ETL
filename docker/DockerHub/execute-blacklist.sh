90cafc1d326087b3fc98f564f2ddfcfc02a53698
Roberto Trujillo <robertotrujillo@MacBook-Pro-de-Roberto-2.local>
2019-04-21 20:06:39 -0500

    Added base artifacts to build the image on docker hub.

---
+++ docker/DockerHub/execute-blacklist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Started ETL Process."
+pwd=$(pwd)
+date=$(date +'%d%m%Y')
+time=$(date +'%H:%M:%S')
+echo "Actual directory: $pwd"
+spark-submit --class com.fexco.poc.ETL --master local[2]  --verbose  --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file://////$pwd/ETLEngine/Log4j.properties -DMY_DATE=$date -DMY_TIME=$time" ETLEngine/aggregationengine_2.11-0.1.jar ETLData/Input/raw-data.csv csv "ETLDataOutput/$date/Blacklist" csv blacklist 100
+echo "Finished ETL Process."
